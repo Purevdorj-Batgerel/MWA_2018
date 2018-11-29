9a2fdbf2eee4b6b51adc787793523901ccf1911e
Purevdorj Batgerel <purvee.hq@gmail.com>
2018-11-29 14:34:38 -0600

    homework 4

--- Assignment3/exercise2.js
+++ Assignment3/exercise2.js
@@ -1,29 +1,29 @@
-    // readFile
+// readFile
 
-    // Stream
+// Stream
 
-    const fs = require('fs');
-    const path = require('path');
-    const http = require('http');
-    const server = http.createServer();
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+const server = http.createServer();
 
-    const port = 3030;
+const port = 3030;
 
-    server.on("request", (req, res) => {
-        res.writeHead(200, {
-            'Content-type': 'text/plain'
-        });
-        const largeFilePath = path.join(__dirname, '200MB.txt');
-
-        // fs.readFile(largeFilePath, (err, data) => {
-        //     if (err) throw err;
-
-        //     res.end(data);
-        // });
-
-        fs.createReadStream(largeFilePath).pipe(res);
-    })
-
-    server.listen(port, () => {
-        console.log(`Server started on port: ${port}`);
+server.on("request", (req, res) => {
+    res.writeHead(200, {
+        'Content-type': 'text/plain'
     });
+    const largeFilePath = path.join(__dirname, '200MB.txt');
+
+    // fs.readFile(largeFilePath, (err, data) => {
+    //     if (err) throw err;
+
+    //     res.end(data);
+    // });
+
+    fs.createReadStream(largeFilePath).pipe(res);
+})
+
+server.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
+});
